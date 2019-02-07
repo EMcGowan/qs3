9854530e72567bb4768f8e94d816e273cb0094b2
EMcGowan <EMcGowan@users.noreply.github.com>
2019-02-07 11:11:18 +0000

    Update GreetingController.java

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -12,7 +12,7 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="Hello World 6") String name) {
+    public Greeting greeting(@RequestParam(value="name", defaultValue="Hello World 7") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
